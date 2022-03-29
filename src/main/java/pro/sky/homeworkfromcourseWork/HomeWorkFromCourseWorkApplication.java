7628062fb4a0d44ff4bba9487c36ce0191ca0a4a
Ksenia <Sheggyshelti@mail.ru>
2022-03-29 07:55:34 +0300

    First

---
+++ src/main/java/pro/sky/homeworkfromcourseWork/HomeWorkFromCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkfromcourseWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkFromCourseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkFromCourseWorkApplication.class, args);
+	}
+
+}
